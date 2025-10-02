b217e6fe9046b8a3135162570f5639fc5f1ddbdc
win <johnwinsmacasaet@gmail.com>
2025-10-02 18:54:00 +0800

    update

--- app/src/main/java/com/example/journal/MainActivity.java
+++ app/src/main/java/com/example/journal/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button signup;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         signup = findViewById(R.id.signup);
+        login = findViewById(R.id.loginbutton);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(MainActivity.this, SignUp.class);
                 Toast.makeText(MainActivity.this, "Register a New Account", Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, home.class);
                 startActivity(intent);
             }
         });

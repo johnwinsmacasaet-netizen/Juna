f0ff0dd3d80c37ce115433fcb098e8aa8846b573
jayl <jeilmacatubal@gmail.com>
2025-11-19 17:13:36 +0800

    updateee

---
+++ app/src/main/java/com/example/journal/journal_item.java
@@ -0,0 +1,14 @@
+package com.example.journal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class journal_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal_item);
+    }
+}
